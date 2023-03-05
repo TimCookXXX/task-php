3649a5567e6cc77e535e25aee5d8f0cd78d8163a
Tim Cook <timleadofficial@gmail.com>
2023-03-05 23:55:14 +0300

    Fix tasks

--- taskFour.php
+++ taskFour.php
@@ -4,13 +4,17 @@ $day = rand(1, 10);
 echo "Сейчас $day день <br>";
 
 switch ($day) {
-  case $day >= 1 && $day <= 5:
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
     echo 'Это рабочий день';
     break;
-  case $day == 6 || $day == 7:
+  case 6:
+  case 7:
     echo 'Это выходной день';
     break;
   default:
     echo 'Неизвестный день';
-    break;
 }
--- taskOne.php
+++ taskOne.php
@@ -3,11 +3,5 @@ $name = 'Tim';
 $age = 23;
 echo "Меня зовут $name <br>";
 echo "Мне $age года <br>";
-echo '" <br />';
-echo "! <br />";
-echo "| <br />";
-echo "/ <br />";
-echo "' <br />";
-echo '" <br />';
-echo "\ <br />";
+echo '"!|/\'"\"';
 ?>
