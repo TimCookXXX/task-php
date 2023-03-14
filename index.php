a710e6e1e012caed4661b3dc7e3d82d6a0c4b061
Tim Cook <timleadofficial@gmail.com>
2023-03-15 00:25:07 +0300

    Success HW3.1

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php 
 include('./src/functions.php');
 
-test();
+for ($i = 0; $i < 50; $i++) {
+  $users[] = createUser(); 
+}
+
+file_put_contents('./users.json', json_encode($users));
+
+$data = file_get_contents('./users.json');
+$decodeUsers = json_decode($data, true);
+
+$names = [];
+$sumAge = 0;
+
+foreach($decodeUsers as $user) {
+  if(isset($names[$user['name']])) {
+    $names[$user['name']]++;
+  } else {
+    $names[$user['name']] = 1;
+  }
+  $sumAge += $user['age'];
+}
+
+var_dump($names);
+echo 'Средний возраст: ' . $sumAge / sizeof($decodeUsers);
 ?>
--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,12 @@
-<?php 
-function test() {
-  echo 'Hello';
+<?php
+function createUser() {
+  $names = ['Tim', 'Andrew', 'Alex', 'Gringo', 'Thomas'];
+  
+  $user = [
+    'name' => $names[array_rand($names)],
+    'age' => mt_rand(18, 45)
+  ];
+
+  return $user;
 }
 ?>
